5d331173b5dccb39e4e9eb3597a0baf2f86756b4
David Pello <dvpg@tuta.io>
2024-10-17 10:09:19 +0200

    Test config file for missing/extra fields

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/BurntSushi/toml"
 )
 
@@ -9,7 +12,7 @@ type Host struct {
 	Username  string `toml:"username"`
 	Password  string `toml:"password"`
 	Hostname  string `toml:"hostname"`
-	IP        string
+	IP        string `toml:"ip"`
 	Offline   string `toml:"offline"`
 }
 
@@ -20,9 +23,34 @@ type Config struct {
 func GetConfig(config_file string) (Config, error) {
 	// parse TOML and return config
 	var config Config
-	_, err := toml.DecodeFile(config_file, &config)
+	tomlData, err := toml.DecodeFile(config_file, &config)
 	if err != nil {
 		return config, err
+	}
+
+	// check for missing configuration values
+	for i, host := range config.Hosts {
+		switch {
+		case host.Hostname == "":
+			return config, errors.New(fmt.Sprintf("Missing 'hostname' in host #%d in configuration file", i+1))
+		case host.UpdateUrl == "":
+			return config, errors.New(fmt.Sprintf("Missing 'update_url' in host #%d in configuration file", i+1))
+		case host.Username == "":
+			return config, errors.New(fmt.Sprintf("Missing 'username' in host #%d in configuration file", i+1))
+		case host.Password == "":
+			return config, errors.New(fmt.Sprintf("Missing 'password' in host #%d in configuration file", i+1))
+		case host.Offline == "":
+			return config, errors.New(fmt.Sprintf("Missing 'offline' in host #%d in configuration file", i+1))
+		}
+	}
+
+	// check for extra configuration values
+	undecoded := tomlData.Undecoded()
+	if len(undecoded) != 0 {
+		for _, u := range undecoded {
+			fmt.Printf("Not recognized entry in configuration file: %s\n", u)
+		}
+		return config, errors.New("Extra data detected")
 	}
 
 	return config, nil
